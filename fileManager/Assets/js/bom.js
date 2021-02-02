aa253c1a180f2d1ee14846879620eb3b939c79ab
Yonas Assefa <se.yonas.assefa@gmail.com>
2021-02-02 11:40:35 -0800

    at this stage, action listener were made and connected to the html element

--- fileManager/Assets/js/bom.js
+++ fileManager/Assets/js/bom.js
@@ -18,22 +18,7 @@ Good Luck !!!
 
 
 // Define UI Variables  here 
-// Href
-// Protocol
-// Host
-// Port
-// Hostname
-
-let locationInfo = document.getElementsByClassName('collection-item');
-
-console.log(locationInfo[1].firstElementChild);
-locationInfo[1].firstElementChild.innerHTML=window.location.href;
-
-
-// locationInfo[1].addEventListener('click',locationInfoDisp);
-
-
-
+let itemArray=document.getElementsByClassName('collection-item');
 
 
 
@@ -42,8 +27,73 @@ locationInfo[1].firstElementChild.innerHTML=window.location.href;
 
 // Display the BOM Information on the innerHTML of the elements
 
-// function locationInfoDisp(e){
-//     console.log(locationInfo.value)
-//     locationInfo[1].innerHTML = 'window.location.href'
 
-// }
+//Location Information -href
+itemArray[0].addEventListener('click',action1);
+function action1(e){
+    console.log(itemArray[0].firstElementChild.innerHTML=window.location.href);
+}
+
+
+//Location Information -protocol
+itemArray[1].addEventListener('click',action2);
+function action2(e){
+    console.log(itemArray[1].firstElementChild.innerHTML=window.location.protocol);
+}
+
+
+//Location Information -host
+itemArray[2].addEventListener('click',action3);
+function action3(e){
+    console.log(itemArray[2].firstElementChild.innerHTML=window.location.host);
+}
+
+
+
+//Location Information -port
+itemArray[3].addEventListener('click',action4);
+function action4(e){
+    console.log(itemArray[3].firstElementChild.innerHTML=window.location.port);
+}
+
+
+//Location Information -hostname
+itemArray[4].addEventListener('click',action5);
+function action5(e){
+    console.log(itemArray[4].firstElementChild.innerHTML=window.location.hostname);
+}
+
+
+//browser info-Appname
+itemArray[5].addEventListener('click',action6);
+function action6(e){
+    console.log(itemArray[5].firstElementChild.innerHTML=window.navigator.appName);
+}
+
+//browser info-AppVersion
+itemArray[6].addEventListener('click',action7);
+function action7(e){
+    console.log(itemArray[6].firstElementChild.innerHTML=window.navigator.appVersion);
+}
+
+
+//browser info-AppVersion
+itemArray[7].addEventListener('click',action8);
+function action8(e){
+    console.log(itemArray[7].firstElementChild.innerHTML=window.navigator.platform);
+}
+
+//browser info-AppVersion
+itemArray[8].addEventListener('click',action9);
+function action9(e){
+    console.log(itemArray[8].firstElementChild.innerHTML=window.navigator.language);
+}
+
+//browser info-AppVersion
+itemArray[9].addEventListener('click',action10);
+function action10(e){
+    console.log(itemArray[9].firstElementChild.innerHTML=window.navigator.cookieEnabled);
+}
+
+
+
