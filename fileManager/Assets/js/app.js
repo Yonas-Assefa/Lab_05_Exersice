3894d17a7c86ce14e3127698d2b75977aac85db4
Yonas Assefa <se.yonas.assefa@gmail.com>
2021-02-02 16:50:11 -0800

    at this stage, on the file manager app, the search/filter functionality were made

--- fileManager/Assets/js/app.js
+++ fileManager/Assets/js/app.js
@@ -10,6 +10,9 @@ const taskList = document.querySelector('.collection');          //The ul
 
 const clearBtn = document.querySelector('.clear-tasks');      //the all task clear button
 
+//the reload button at the top right of navigation
+const reloadIcon = document.querySelector('.fa'); 
+
 // Add Event Listener  [Form , clearBtn and filter search input ]
 
 // form submit 
@@ -21,47 +24,116 @@ clearBtn.addEventListener('click', clearAllTasks);
 //   Filter Task 
 filter.addEventListener('keyup', filterTasks);
 
+//   Filter Task 
+filter.addEventListener('keypress', compare);
 
-// Add New  Task Function definition 
-// function addNewTask(e) {
+// Remove task event [event delegation]
+taskList.addEventListener('click', removeTask);
 
-//     alert("Add New Task ....");
+// Event Listener for reload 
+reloadIcon.addEventListener('click', reloadPage);
 
-//     e.preventDefault(); //disable form submission
-// }
+  
 
-// Add New  Task Function definition 
+
+
+
 function addNewTask(e) {
+    if (taskInput.value == '') 
+    {
+       taskInput.style.borderColor = "red";
+       return;
+    }
+    else{
+        // Create an li element when the user adds a task 
+        const li = document.createElement('li');
+        // Adding a class
+        li.className = 'collection-item';
+        // Create text node and append it 
+        li.appendChild(document.createTextNode(taskInput.value));
+        // Create new element for the link 
+        const link = document.createElement('a');
+        // Add class and the x marker for a 
+        link.className = 'delete-item secondary-content';
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+        // Append link to li
+        li.appendChild(link);
+        // Append to ul 
+        taskList.appendChild(li);
+    }
 
-  // Create an li element when the user adds a task 
-  const li = document.createElement('li');
-  // Adding a class
-  li.className = 'collection-item';
-  // Create text node and append it 
-  li.appendChild(document.createTextNode(taskInput.value));
-  // Create new element for the link 
-  const link = document.createElement('a');
-  // Add class and the x marker for a 
-  link.className = 'delete-item secondary-content';
-  link.innerHTML = '<i class="fa fa-remove"></i>';
-  // Append link to li
-  li.appendChild(link);
-  // Append to ul 
-  taskList.appendChild(li);
+  
 
   e.preventDefault(); //disable form submission
 }
 
 
+
 // Clear Task Function definition 
 function clearAllTasks() {
 
-    alert("Clear tasks ....");
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+
 
 }
+
+var searchWord;
+
 // Filter tasks function definition 
 function filterTasks(e) {
 
-    console.log("Task Filter ...");
+    //console.log("Task Filter ...");
+   // console.log(e.target.value);
+     searchWord=e.target.value;
+    
+    tasksList=document.querySelectorAll('.collection-item');
+    //console.log(tasksList);
+    // // console.log(tasksList[1].innerText);
+   
 
 }
+
+function compare(e){
+    if(e.key==='Enter'){
+        //console.log(searchWord)
+        tasksList.forEach(compare1);
+        function compare1(item){
+        let itemValue=item.innerText;
+        console.log(item)
+        if(itemValue==searchWord){
+            item.style.display='block'
+
+        }
+        else{item.style.display='none';
+        
+    }
+        
+    }
+    }
+}
+ 
+
+
+// Remove Task function definition 
+function removeTask(e) {
+
+    if (e.target.parentElement.classList.contains('delete-item'))
+        {
+        if (confirm('Are You Sure about that ?'))
+        {
+            e.target.parentElement.parentElement.remove();
+        }
+
+    }
+
+}
+
+
+// Reload Page Function 
+function reloadPage() {
+    //using the reload fun on location object 
+    location.reload();
+}
+
--- fileManager/Assets/js/bom.js
+++ fileManager/Assets/js/bom.js
@@ -31,21 +31,21 @@ let itemArray=document.getElementsByClassName('collection-item');
 //Location Information -href
 itemArray[0].addEventListener('click',action1);
 function action1(e){
-    console.log(itemArray[0].firstElementChild.innerHTML=window.location.href);
+    itemArray[0].firstElementChild.innerHTML=window.location.href;
 }
 
 
 //Location Information -protocol
 itemArray[1].addEventListener('click',action2);
 function action2(e){
-    console.log(itemArray[1].firstElementChild.innerHTML=window.location.protocol);
+    itemArray[1].firstElementChild.innerHTML=window.location.protocol;
 }
 
 
 //Location Information -host
 itemArray[2].addEventListener('click',action3);
 function action3(e){
-    console.log(itemArray[2].firstElementChild.innerHTML=window.location.host);
+    itemArray[2].firstElementChild.innerHTML=window.location.host;
 }
 
 
@@ -53,78 +53,78 @@ function action3(e){
 //Location Information -port
 itemArray[3].addEventListener('click',action4);
 function action4(e){
-    console.log(itemArray[3].firstElementChild.innerHTML=window.location.port);
+    itemArray[3].firstElementChild.innerHTML=window.location.port;
 }
 
 
 //Location Information -hostname
 itemArray[4].addEventListener('click',action5);
 function action5(e){
-    console.log(itemArray[4].firstElementChild.innerHTML=window.location.hostname);
+    itemArray[4].firstElementChild.innerHTML=window.location.hostname;
 }
 
 
 //browser info-Appname
 itemArray[5].addEventListener('click',action6);
 function action6(e){
-    console.log(itemArray[5].firstElementChild.innerHTML=window.navigator.appName);
+    itemArray[5].firstElementChild.innerHTML=window.navigator.appName;
 }
 
 //browser info-AppVersion
 itemArray[6].addEventListener('click',action7);
 function action7(e){
-    console.log(itemArray[6].firstElementChild.innerHTML=window.navigator.appVersion);
+    itemArray[6].firstElementChild.innerHTML=window.navigator.appVersion;
 }
 
 
 //browser info-AppVersion
 itemArray[7].addEventListener('click',action8);
 function action8(e){
-    console.log(itemArray[7].firstElementChild.innerHTML=window.navigator.platform);
+    itemArray[7].firstElementChild.innerHTML=window.navigator.platform;
 }
 
 //browser info-AppVersion
 itemArray[8].addEventListener('click',action9);
 function action9(e){
-    console.log(itemArray[8].firstElementChild.innerHTML=window.navigator.language);
+    itemArray[8].firstElementChild.innerHTML=window.navigator.language;
 }
 
 //browser info-AppVersion
 itemArray[9].addEventListener('click',action10);
 function action10(e){
-    console.log(itemArray[9].firstElementChild.innerHTML=window.navigator.cookieEnabled);
+    itemArray[9].firstElementChild.innerHTML=window.navigator.cookieEnabled;
 }
 
 
 //screen info-height
 itemArray[10].addEventListener('click',action11);
 function action11(e){
-    console.log(itemArray[10].firstElementChild.innerHTML=window.screen.height);
+    itemArray[10].firstElementChild.innerHTML=window.screen.height;
 }
 
 //screen info-width
 itemArray[11].addEventListener('click',action12);
 function action12(e){
-    console.log(itemArray[11].firstElementChild.innerHTML=window.screen.width);
+    itemArray[11].firstElementChild.innerHTML=window.screen.width;
 }
 
 //screen info-pixldebth
 itemArray[12].addEventListener('click',action13);
 function action13(e){
-    console.log(itemArray[12].firstElementChild.innerHTML=window.screen.pixelDepth);
+    itemArray[12].firstElementChild.innerHTML=window.screen.pixelDepth;
 }
 
 
 //browsing-history info--length
 itemArray[13].addEventListener('click',action14);
 function action14(e){
-    console.log(itemArray[13].firstElementChild.innerHTML=window.history.length);
+    itemArray[13].firstElementChild.innerHTML=window.history.length;
 }
 
 //browsing-history info--state
 itemArray[14].addEventListener('click',action15);
 function action15(e){
-    console.log(itemArray[14].firstElementChild.innerHTML=window.history.state);
+    itemArray[14].firstElementChild.innerHTML=window.history.state;
 }
 
 
